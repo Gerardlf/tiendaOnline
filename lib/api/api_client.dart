393b19b0ca6e15fd9df87d13a40863c27ad4f979
Gerardo de la fuente <gerdeluri@alu.edu.gva.es>
2026-01-17 14:43:42 +0100

    creando api_client

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -1,3 +1,10 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
+import '../data/product.dart';
+
 class ApiClient {
   final String baseUrl;
 
@@ -5,7 +12,28 @@ class ApiClient {
 
   //obtenemos la lista de productos
 
-  Future<List<Product>> fetchProducts() async {
+  Future<List<Product>> fetchProducts({String? category}) async {
     Uri uri;
+
+    //construimos la uri dependiendo si hay categoria o no
+
+    if (category != null && category.isNotEmpty) {
+      uri = Uri.parse('$baseUrl/products/category/$Category');
+    } else {
+      uri = Uri.parse('$baseUrl/products');
+    }
+
+    final respuesta = await http.get(uri);
+
+    if (respuesta.statusCode != 200) {
+      throw Exception(
+        'Error al cargar los productos \n Error: ${respuesta.statusCode}',
+      );
+    }
+
+    final datos = jsonDecode(respuesta.body) as List;
+    return datos
+        .map((element) => Product.fromJson(element as Map<String, dynamic>))
+        .toList();
   }
 }
