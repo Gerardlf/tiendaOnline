693faba85f588ebf3047de132038b17a2d6ce154
Gerardo de la fuente <gerdeluri@alu.edu.gva.es>
2026-01-17 19:01:15 +0100

    Creando una nuevo cardProduct para la API

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -117,8 +117,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         );
                       },
                     ),
-                  );
-                  */
+                  );*/
                 },
               ),
       ),
---
+++ lib/widgets/product_card_api.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class ProductCardApi extends StatelessWidget {
+  final int id;
+  final String title;
+  final double price;
+  final String category;
+  final String imageUrl;
+
+  final VoidCallback onAdd;
+  final VoidCallback? onTap;
+
+  const ProductCardApi({
+    super.key,
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.category,
+    required this.imageUrl,
+    required this.onAdd,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shadowColor: Colors.blueAccent,
+      elevation: 12,
+      child: ListTile(
+        onTap: onTap,
+        leading: Image.network(
+          imageUrl,
+          width: 53,
+          height: 53,
+          fit: BoxFit.cover,
+          errorBuilder: (_, _, _) => const Icon(
+            Icons.image_not_supported,
+            size: 40,
+            color: Colors.indigo,
+          ),
+        ),
+        title: Text(
+          title,
+          maxLines: 3,
+          overflow: TextOverflow.ellipsis,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+            fontSize: 18,
+          ),
+        ),
+        subtitle: Text(
+          "$category\n${price.toStringAsFixed(2)} €",
+          style: TextStyle(color: Colors.blueGrey[700], fontSize: 14),
+        ),
+        trailing: ElevatedButton(
+          style: ElevatedButton.styleFrom(backgroundColor: Colors.blueAccent),
+          onPressed: onAdd,
+          child: const Text("Añadir", style: TextStyle(color: Colors.white)),
+        ),
+      ),
+    );
+  }
+}
